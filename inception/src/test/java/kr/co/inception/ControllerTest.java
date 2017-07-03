772b41f341205d51fbbdd986e3df117a2d94e078
KIMTYNOTEBOOK\KimTY <ktts584@gmail.com>
2017-07-03 10:39:47 +0900

    패키징

--- inception/src/test/java/kr/co/inception/ControllerTest.java
+++ inception/src/test/java/kr/co/inception/ControllerTest.java
@@ -37,24 +37,24 @@ public class ControllerTest {
 
 //	@Test
 //	public void userjoin() throws Exception{
-//		System.out.println("회원가입 테스트");
-//		mockMvc.perform(MockMvcRequestBuilders.post("/user/joinchk").param("userid","kkb1").param("userpw","312").param("useremail","asd@gmail.com").param("profilepicture","kkb.jpg").param("sex", "남자")).andDo(print());
+//		System.out.println("�쉶�썝媛��엯 �뀒�뒪�듃");
+//		mockMvc.perform(MockMvcRequestBuilders.post("/user/joinchk").param("userid","kkb1").param("userpw","312").param("useremail","asd@gmail.com").param("profilepicture","kkb.jpg").param("sex", "�궓�옄")).andDo(print());
 //	}
 	
 //	@Test
 //	public void userlogin() throws Exception{
-//		System.out.println("로그인 테스트");
+//		System.out.println("濡쒓렇�씤 �뀒�뒪�듃");
 //		mockMvc.perform(MockMvcRequestBuilders.post("/user/loginchk").param("userid","kkb1").param("userpw","312")).andDo(print());
 //	}
 //	@Test
 //	public void updateUser() throws Exception{
-//		System.out.println("회원정보수정 테스트");
-//		mockMvc.perform(MockMvcRequestBuilders.post("/user/updatechk").param("userid","kkb1").param("userpw","33333").param("profilepicture","두번째로바꿈").param("useremail","또바꿈").param("sex","여자")).andDo(print());
+//		System.out.println("�쉶�썝�젙蹂댁닔�젙 �뀒�뒪�듃");
+//		mockMvc.perform(MockMvcRequestBuilders.post("/user/updatechk").param("userid","kkb1").param("userpw","33333").param("profilepicture","�몢踰덉㎏濡쒕컮轅�").param("useremail","�삉諛붽퓞").param("sex","�뿬�옄")).andDo(print());
 //	}
-//	@Test
-//	public void movepageJoinform() throws Exception{
-//		System.out.println("회원가입화면으로로 이동");
-//		mockMvc.perform(MockMvcRequestBuilders.post("/user/join")).andDo(print());
-//	}
+	@Test
+	public void movepageJoinform() throws Exception{
+		System.out.println("�쉶�썝媛��엯�솕硫댁쑝濡쒕줈 �씠�룞");
+		mockMvc.perform(MockMvcRequestBuilders.post("/user/join")).andDo(print());
+	}
 
 }
