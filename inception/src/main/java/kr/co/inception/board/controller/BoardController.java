37fc059d4eff5c4f754c1535a8e41b30c3caae34
kabum6612 <kabum6612@gmail.com>
2017-07-07 14:14:35 +0900

    안드로이드 게시판전체보기

--- inception/src/main/java/kr/co/inception/board/controller/BoardController.java
+++ inception/src/main/java/kr/co/inception/board/controller/BoardController.java
@@ -1,8 +1,38 @@
 package kr.co.inception.board.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.inception.board.service.BoardService;
+import kr.co.inception.board.vo.BoardListVO;
+
+
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
+	
+	private Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@Autowired
+	private BoardService boardService;
+	
+	
+	//안드용 json
+		@RequestMapping(value = "/andboardlist")
+		@ResponseBody
+		public List<BoardListVO> boardListJson(Model model){
+			List<BoardListVO> boardListVO = boardService.showBoardList();
+			
+			
+			return boardListVO;
+		}
 
 }
