b579df164e525271816affc032c0adf20b2dcc3e
DESKTOP-NVGKVQU\Bit <ktts584@gmail.com>
2017-07-15 17:31:22 +0900

    no message

--- inception/src/main/java/kr/co/inception/board/controller/BoardController.java
+++ inception/src/main/java/kr/co/inception/board/controller/BoardController.java
@@ -8,13 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.inception.board.dto.BoardInsertDTO;
+import kr.co.inception.board.dto.BoardUpdateDTO;
+import kr.co.inception.board.dto.GoodDTO;
+import kr.co.inception.board.dto.ReplyDTO;
+import kr.co.inception.board.dto.ScrapeDTO;
 import kr.co.inception.board.service.BoardService;
 import kr.co.inception.board.vo.BoardDetailVO;
 import kr.co.inception.board.vo.BoardListVO;
+import kr.co.inception.board.vo.BoardSimpleVO;
 
 
 
@@ -49,10 +55,49 @@ public class BoardController {
 		
 		@RequestMapping(value = "/boardInsert")
 		public String boardInsert(BoardInsertDTO boardInsertDTO,Model model){
-			BoardDetailVO boardDetailVO = boardService.showBoardDetail(boardService.boardInsert(boardInsertDTO));
+			boardService.boardInsert(boardInsertDTO);
 			
-			
-			return "showBoardDeteil";
+			return "/boardList";
 		}
+		
+		@RequestMapping(value = "/boardUpdate")
+		public String boardUpdate(BoardUpdateDTO boardUpdateDTO){
+			
+			boardService.boardUpdate(boardUpdateDTO);
+			
+			return "/boardList";
+		}
+		
+		@RequestMapping(value = "/replyInsert")
+		public String replyInsert(ReplyDTO replyDTO,Model model){
+			
+			boardService.replyInsert(replyDTO);
+			
+			return "/boardSimple";
+		}
+		
+		@RequestMapping(value = "/scrape")
+		public String scrape(ScrapeDTO scrapeDTO){
+			
+			boardService.Scrape(scrapeDTO);
+			
+			return "/boardList";
+		}
+		
+		@RequestMapping(value = "/good")
+		public String good(GoodDTO goodDTO){
+			boardService.good(goodDTO);
+			
+			return "/boardSimple";
+		}
+		
+		
+		@RequestMapping(value = "/boardSimple",method = RequestMethod.GET)
+		public String boardSimple(String bidx,Model model){
+			BoardSimpleVO boardSimpleVO= boardService.showBoardSimple(bidx);
+			model.addAttribute(boardSimpleVO);
+			return "BoardSimple";
+		}
+		
 		
 }
