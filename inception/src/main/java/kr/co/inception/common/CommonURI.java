23c985d1e2ad032e44a1b499e83bb7449d2dac7a
DESKTOP-NVGKVQU\Bit <ktts584@gmail.com>
2017-08-07 13:03:49 +0900

    no message

---
+++ inception/src/main/java/kr/co/inception/common/CommonURI.java
@@ -0,0 +1,36 @@
+package kr.co.inception.common;
+
+public class CommonURI {
+	public static String board="/board";
+	public static String user="/user";
+	
+	
+	public static String android;
+	public static String hottagList="/hottagList";
+	public static String boardList="/boardList";
+	public static String boardDetail="/boardDetail";
+	public static String boardInsert="/boardInsert";
+	public static String boardUpdate="/boardUpdate";
+	public static String reply="/reply";
+	public static String write="/write";
+	public static String scrape="/scrape";
+	public static String good="/good";
+	public static String bad="/bod";
+	public static String uploadImage="/uploadImage";
+	
+	
+	public static String follow="/follow";
+	public static String followcheck="/followcheck";
+	public static String followUser="/follow";
+	public static String unfollowUser="/unfollow";
+
+	public static String rood="/";
+	public static String mainCategory="/category";
+	
+	
+	public static String profile="/profile";
+	public static String profileBoard="/{param1}/board2";
+	public static String profilereply="/{param1}/reply2";
+	
+
+}
